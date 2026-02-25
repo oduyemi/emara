0301f00e8ec47ad07c6d40e9c17a2d0a73f17a77
Yemi Oduyemi <yhermii@gmail.com>
2026-02-25 20:26:25 +0100

    Updated

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -2,6 +2,8 @@ import AboutHero from "@/components/about/Hero";
 import AboutIntro from "@/components/about/Intro";
 import VisionMission from "@/components/about/VisionMission";
 import CoreValues from "@/components/about/CoreValues";
+import { FAQ } from "@/components/home/FAQ";
+import { HomeCTA } from "@/components/home/CTA";
 
 export default function AboutPage() {
   return (
@@ -10,6 +12,8 @@ export default function AboutPage() {
       <AboutIntro />
       <VisionMission />
       <CoreValues />
+      <FAQ />
+      <HomeCTA />
     </main>
   );
 }
